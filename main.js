89e6551600d93bef7d4dad855eff96d1315afc97
DavidMayorgaS08 <david08102@hotmail.com>
2023-11-20 15:18:35 -0500

    Avances trabajos

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     document.getElementById("loader").classList.remove("loader2");
 
-    setTimeout(function() {
+    setTimeout(function () {
         document.getElementById("loader").classList.add("loader2");
 
-        setTimeout(function() {
+        setTimeout(function () {
             document.getElementById("loader").classList.add("loader2");
         }, 1000);
     }, 2000);
 });
+
+function verLista() {
+    let lista = document.querySelector(".cont_listado");
+    let check = document.querySelector("#check");
+
+    if (check.checked) {
+        lista.style.right = "4%";
+    } else {
+        lista.style.right = "-15%";
+    }
+}
+
+function volver() {
+
+}
+
+function verTrabajos() {
+    let titulos = document.querySelector(".cont_titulos");
+    titulos.style.display = "none";
+
+    let trabajos = document.querySelector(".cont_proyectos_new");
+    trabajos.style.display = "flex";
+
+    let lista = document.querySelector(".cont_listado");
+    lista.style.right = "-15%";
+
+    let previews = document.querySelector(".cont_previews");
+    previews.style.display = "flex";
+}
+
+function verProyectos() {
+
+}
+
+function preview() {
+
+}
